38344fa767ad69f95113e1fa2fc5222a6e8c7bd9
Luftwaff1e <kcarlstr@cs.ualberta.ca>
2015-01-21 15:02:53 -0700

    No longer closes app when you hit save.

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -23,6 +23,8 @@ public class LonelyTwitterActivity extends Activity {
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
+	private ArrayList<String> tweets;
+	private ArrayAdapter<String> adapter;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -39,34 +41,25 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
+				tweets.add(text);
 				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
-
+				adapter.notifyDataSetChanged();
 			}
+			
 		});
 	}
 
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
-		User u = new Author();
-		
-		try {
-			u.setName("Joe");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		ArrayList<User> users = new ArrayList<User>();
-		
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		tweets = loadFromFile();
+		adapter = new ArrayAdapter<String>(this,
 				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private String[] loadFromFile() {
+	private ArrayList<String> loadFromFile() {
 		ArrayList<String> tweets = new ArrayList<String>();
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
@@ -84,7 +77,7 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+		return tweets;
 	}
 	
 	private void saveInFile(String text, Date date) {
